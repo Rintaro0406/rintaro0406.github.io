8876c77ef65d9378a8222bab6297dcff33eb8dbd
Rintaro Kanaki <R.Kanaki@campus.lmu.de>
2025-05-29 04:44:34 +0100

    Added cat picture

--- js/translate.js
+++ js/translate.js
@@ -1,7 +1,15 @@
 const translations = {
   en: {
     cv: "CV",
-    cv_text: "Download my latest CV here.",
+    cv_text: `
+<strong>🎓 Education:</strong><br>
+PhD Newcastle 🇬🇧 | MSc LMU 🇩🇪 | Exchange Bologna 🇮🇹 | BSc Bayreuth 🇩🇪<br>
+<strong>🔬 Research:</strong><br>
+Bayesian photo-z | Weak lensing GNN | MRI disk modeling<br>
+<strong>💼 Experience:</strong><br>
+Zeiss Meditec 🇩🇪 | NGO Volunteer 🇺🇦<br>
+<strong>🧑‍🏫 Teaching:</strong> 🇬🇧🇩🇪 | <strong>💬 Languages:</strong> 🇯🇵🇩🇪🇬🇧🇮🇹
+`,
     research: "Research",
     research_text: "My current research focuses on cosmological applications of statistical methods, particularly spatial modelling, machine learning, and Bayesian inference.",
     blog: "Blog (Hobby)",
@@ -13,7 +21,15 @@ const translations = {
   },
   ja: {
     cv: "履歴書",
-    cv_text: "最新の履歴書は<a href='assets/Rintaro_Kanaki_CV.pdf'>こちら</a>からダウンロードできます。",
+    cv_text: `
+<strong>🎓 学歴:</strong><br>
+博士🇬🇧 | 修士🇩🇪 | 交換留学🇮🇹 | 学士🇩🇪<br>
+<strong>🔬 研究:</strong><br>
+ベイズ的photo-z推定 | 弱重力レンズGNN | MRI円盤モデル<br>
+<strong>💼 経験:</strong><br>
+ツァイス🇩🇪 | NGOボランティア🇺🇦<br>
+<strong>🧑‍🏫 教育:</strong> 🇬🇧🇩🇪 | <strong>💬 言語:</strong> 🇯🇵🇩🇪🇬🇧🇮🇹
+`,
     research: "研究",
     research_text: "現在は、宇宙論的な統計手法の応用、特に空間モデリング、機械学習、ベイズ推論に焦点を当てています。",
     blog: "ブログ（趣味）",
@@ -25,7 +41,15 @@ const translations = {
   },
   de: {
     cv: "Lebenslauf",
-    cv_text: "Laden Sie meinen aktuellen Lebenslauf <a href='assets/Rintaro_Kanaki_CV.pdf'>hier</a> herunter.",
+    cv_text: `
+<strong>🎓 Ausbildung:</strong><br>
+PhD Newcastle 🇬🇧 | MSc LMU 🇩🇪 | Austausch Bologna 🇮🇹 | BSc Bayreuth 🇩🇪<br>
+<strong>🔬 Forschung:</strong><br>
+Bayes. Photo-z | Schwache Linsen GNN | MRI Scheibenmodellierung<br>
+<strong>💼 Erfahrung:</strong><br>
+Zeiss Meditec 🇩🇪 | NGO Helfer 🇺🇦<br>
+<strong>🧑‍🏫 Lehre:</strong> 🇬🇧🇩🇪 | <strong>💬 Sprachen:</strong> 🇯🇵🇩🇪🇬🇧🇮🇹
+`,
     research: "Forschung",
     research_text: "Meine aktuelle Forschung befasst sich mit der Anwendung kosmologischer statistischer Methoden, insbesondere der räumlichen Modellierung, des maschinellen Lernens und der bayesianischen Inferenz.",
     blog: "Blog (Hobby)",
